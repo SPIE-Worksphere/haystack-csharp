2ab79cf95251c604a823763709b4f5aacc0a322e
Ivo Brugman <ivo.brugman@strukton.com>
2019-03-28 14:07:52 +0100

    Ensure Uri's end with a slash when using HAsyncClient and/or AsyncAuthClientContext.

---
+++ ProjectHaystack/Util/UriExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectHaystack.Util
+{
+    public static class UriExtensions
+    {
+        public static Uri EndWithSlash(this Uri uri)
+        {
+            if (!uri.AbsolutePath.EndsWith("/"))
+            {
+                var builder = new UriBuilder(uri);
+                builder.Path += "/";
+                uri = builder.Uri;
+            }
+
+            return uri;
+        }
+    }
+}
