74a7aee7fff276779a8d819835e4719faf5b32da
Chris Breederveld <chris.breederveld@strukton.com>
2021-10-25 20:27:06 +0000

    Added unit tests and small fix for CallAsync changes

--- ProjectHaystackTest/Client/HaystackClientTest.cs
+++ ProjectHaystackTest/Client/HaystackClientTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ProjectHaystack;
@@ -69,6 +70,70 @@ namespace ProjectHaystackTest.Client.Tests
             var uri = new Uri("http://localhost:8080/api/demo");
             var client = new HaystackClient(_user, _pass, uri);
             Assert.AreEqual(client.Uri.AbsoluteUri[client.Uri.AbsoluteUri.Length - 1], '/');
+        }
+
+        [TestMethod]
+        public async Task CallAsync_NoGrid_CallsWithGet()
+        {
+            HttpRequestMessage lastRequest = null;
+            var httpClient = new HttpClientMockBuilder(_uri)
+                .WithBasicAuthentication(_basicAuthHash)
+                .WithRequestHandler(request =>
+                {
+                    lastRequest = request;
+                    return Task.FromResult(new HttpResponseMessage { Content = new StringContent("ver:\"3.0\"\nempty\n") });
+                })
+                .Build();
+            var client = new HaystackClient(httpClient, new BasicAuthenticator(_user, _pass), _uri);
+            await client.OpenAsync();
+
+            await client.CallAsync("ops");
+
+            Assert.AreEqual(HttpMethod.Get, lastRequest.Method);
+            Assert.AreEqual("/api/demo/ops", lastRequest.RequestUri.AbsolutePath);
+        }
+
+
+        [TestMethod]
+        public async Task CallAsync_EmptyGrid_CallsWithGet()
+        {
+            HttpRequestMessage lastRequest = null;
+            var httpClient = new HttpClientMockBuilder(_uri)
+                .WithBasicAuthentication(_basicAuthHash)
+                .WithRequestHandler(request =>
+                {
+                    lastRequest = request;
+                    return Task.FromResult(new HttpResponseMessage { Content = new StringContent("ver:\"3.0\"\nempty\n") });
+                })
+                .Build();
+            var client = new HaystackClient(httpClient, new BasicAuthenticator(_user, _pass), _uri);
+            await client.OpenAsync();
+
+            await client.CallAsync("ops", HaystackGrid.Empty);
+
+            Assert.AreEqual(HttpMethod.Get, lastRequest.Method);
+            Assert.AreEqual("/api/demo/ops", lastRequest.RequestUri.AbsolutePath);
+        }
+
+        [TestMethod]
+        public async Task CallAsync_FilledGrid_CallsWithPost()
+        {
+            HttpRequestMessage lastRequest = null;
+            var httpClient = new HttpClientMockBuilder(_uri)
+                .WithBasicAuthentication(_basicAuthHash)
+                .WithRequestHandler(request =>
+                {
+                    lastRequest = request;
+                    return Task.FromResult(new HttpResponseMessage { Content = new StringContent("ver:\"3.0\"\nempty\n") });
+                })
+                .Build();
+            var client = new HaystackClient(httpClient, new BasicAuthenticator(_user, _pass), _uri);
+            await client.OpenAsync();
+
+            await client.CallAsync("ops", new HaystackGrid().AddColumn("key").AddRow(new HaystackString("value")));
+
+            Assert.AreEqual(HttpMethod.Post, lastRequest.Method);
+            Assert.AreEqual("/api/demo/ops", lastRequest.RequestUri.AbsolutePath);
         }
     }
 }
