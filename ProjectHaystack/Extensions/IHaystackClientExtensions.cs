94870ae0117597fd032b217e0ab79b3691066328
Ivo Brugman <ivo.brugman.strukton@gmail.com>
2023-02-03 15:40:22 +0100

    Add CancellationToken support

--- ProjectHaystack/Extensions/IHaystackClientExtensions.cs
+++ ProjectHaystack/Extensions/IHaystackClientExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System.Threading;
+using System.Threading.Tasks;
 
 namespace ProjectHaystack.Client
 {
@@ -10,10 +11,18 @@ namespace ProjectHaystack.Client
         /// <param name="id">Record ID.</param>
         /// <param name="items">Time-series data.</param>
         public static Task<HaystackGrid> HisWriteNoWarnAsync(this IHaystackClient client, HaystackReference id, HaystackHistoryItem[] items)
+            => HisWriteNoWarnAsync(client, id, items, CancellationToken.None);
+
+        /// <summary>
+        /// Convenience method to call HisWriteNoWarnAsync with a "noWarn" marker to
+        /// prevent warnings when writing out-of-order data.
+        /// <param name="id">Record ID.</param>
+        /// <param name="items">Time-series data.</param>
+        public static Task<HaystackGrid> HisWriteNoWarnAsync(this IHaystackClient client, HaystackReference id, HaystackHistoryItem[] items, CancellationToken cancellationToken)
         {
             var meta = new HaystackDictionary();
             meta.Add("noWarn", new HaystackMarker());
-            return client.HisWriteAsync(id, items, meta);
+            return client.HisWriteAsync(id, items, cancellationToken, meta);
         }
     }
 }
